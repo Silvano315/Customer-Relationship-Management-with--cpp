32def8e31ab280b70e94dfd52959e63a20960927
Silvano Quarto <silvano.quarto@gmail.com>
2024-07-25 16:07:11 +0200

    cpp file Interaction uploaded

---
+++ src/Interaction.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "Interaction.h"
+
+Interaction::Interaction(const string& _type, const string& _date, const string& _note){
+    
+    type = _type;
+    date = _date;
+    note = _note;
+}
+
+void Interaction::view_interaction_details () const{
+
+    cout << "Interaction Type: " << type << endl;
+    cout << "Date: " << date << endl;
+    cout << "Note: " << note << endl;
+};
