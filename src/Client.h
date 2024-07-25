9d685d4ac1f54a8e92c093fafa094e91fc30bb64
Silvano Quarto <silvano.quarto@gmail.com>
2024-07-25 15:20:08 +0200

    h file for Client updated

---
+++ src/Client.h
@@ -0,0 +1,41 @@
+/*
+Here, I define class Client and the operations between clients.
+
+Firstly, I need to prevent duplication about the inclusion of header file. I need this content to be included only once.
+I used ifndef/endif and define.
+
+
+
+*/
+
+
+#ifndef CLIENT.H
+#define CLIENT.H
+
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+class Client
+{
+public:
+    string name;
+    string surname;
+    string phone;
+    string email;
+    string address;
+    string city;
+    //string role;
+    //string company; oppure bool company;
+
+    // I need a specific constructor with input by reference (more efficient and direct) so to also change original variable
+    Client(const string& _name, const string& _surname, const string& _phone, const string& _email, const string& _address, const string& _city){
+
+        // Here I need to implement interaction operations (add and view) and client operation (edit and view)
+
+    };
+    ~Client();
+};
+
+#endif
