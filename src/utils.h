77ea25f59955f762d5709dbf69a98126b03657f2
Silvano Quarto <silvano.quarto@gmail.com>
2024-07-26 17:08:02 +0200

    h file for utils uploaded

---
+++ src/utils.h
@@ -0,0 +1,22 @@
+/*
+Here, I define functions for general purpose to handle manu and user's input.
+
+Firstly, I need to prevent duplication about the inclusion of header file. I need this content to be included only once.
+I used ifndef/endif and define.
+
+
+
+*/
+
+
+#ifndef UTILS.H
+#define UTILS.H
+
+#include "Clients_Management.h"
+
+
+void show_menu();
+void user_input(ClientsManagement& _management);
+
+
+#endif
