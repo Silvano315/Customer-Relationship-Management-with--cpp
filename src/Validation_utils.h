a1401a67ff36595bffd8d5f23cb45f4a9abd6909
Silvano Quarto <silvano.quarto@gmail.com>
2024-07-31 12:14:39 +0200

    validation fx header file

---
+++ src/Validation_utils.h
@@ -0,0 +1,13 @@
+#ifndef VALIDATIONUTILS_H
+#define VALIDATIONUTILS_H
+
+#include <bits/stdc++.h>
+using namespace std;
+#include <string>
+#include <regex>
+
+bool validate_string(const string& str);
+bool validate_phone(const string& phone);
+bool validate_email(const string& email);
+
+#endif // VALIDATIONUTILS_H
